e9103aef1ab0946fc2daf64def4bfd5fe57e0275
Sam Gubernikoff <sgubernikoff@gmail.com>
2022-04-15 11:07:23 -0400

    updated backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
+  resources :users
+  resources :purchases, only: [:create, :index]
+  resources :items, only: [:index, :show]
   
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  # get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
