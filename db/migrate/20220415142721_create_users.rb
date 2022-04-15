7bd997ad10abca52ff4722fb08f6f36fd1642f03
Zelie Chowaiki <zeliechowaiki@gmail.com>
2022-04-15 13:34:22 -0400

    items seeding

--- db/migrate/20220415142721_create_users.rb
+++ db/migrate/20220415142721_create_users.rb
@@ -3,7 +3,6 @@ class CreateUsers < ActiveRecord::Migration[6.1]
     create_table :users do |t|
       t.string :name
       t.string :password
-      t.string :id
       t.integer :points
       t.string :username
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "🌱 Seeding spices..."
+
+10.times do
+  Item.create(
+    name: Faker::Commerce.product_name,
+    description: Faker::Commerce.color,
+    price: rand(20..200),
+    image: Faker::Avatar.image
+  )
+end
+
+puts "✅ Done seeding!"
