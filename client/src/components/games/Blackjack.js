3d0a78759800ba2c34b4f1d86100d827fea8c732
Zelie Chowaiki <zeliechowaiki@gmail.com>
2022-04-19 09:11:26 -0400

    blackjack done and some roulette

--- client/src/components/games/Blackjack.js
+++ client/src/components/games/Blackjack.js
@@ -1,4 +1,4 @@
-import {React, useState, useEffect} from 'react';
+import {React, useState} from 'react';
 import { v4 as uuid } from 'uuid';
 import timeout from "../timeout";
 
@@ -24,45 +24,112 @@ function Blackjack() {
     });
   }
 
-  function cardValueConvert(card) {
+  function cardValueConvert(card, noAceValue) {
     if (card === "ACE") {
-      if (playerCardsValue <= 10) return 11;
+      if (noAceValue <= 10) return 11;
       else return 1;
     }
     else if (!parseInt(card)) return 10
     else return parseInt(card);
   }
 
-  function stand() {
+  function getHandValue(hand) {
+    let handAces = hand.filter(card => card.value === "ACE");
+    if (handAces.length > 1) {
+      handAces.slice(1).forEach(card => card.value = "1");
+    }
+    let cardsValue = hand.filter(card => card.value !== "ACE").map(card => cardValueConvert(card.value, 0)).reduce((a,b) => a+b, 0);
+    cardsValue = cardsValue + hand.filter(card => card.value === "ACE").map(card => cardValueConvert(card.value, cardsValue)).reduce((a,b) => a+b, 0);
+    return cardsValue;
+  }
+
+  function hitDealer () {
     if (houseCardsValue <= 16) {
-      fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
+      fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=3`)
       .then(response => response.json())
       .then(cardData => {
-      setHouseCards([...houseCards, cardData.cards[0]]);
+        if (getHandValue([...houseCards, cardData.cards[0]]) >= 17) {
+          setHouseCards([...houseCards, cardData.cards[0]]);
+        }
+        else if (getHandValue([...houseCards, cardData.cards[0], cardData.cards[1]]) >= 17) {
+          setHouseCards([...houseCards, cardData.cards[0], cardData.cards[1]]);
+        }
+        else {
+          setHouseCards([...houseCards, cardData.cards[0], cardData.cards[1], cardData.cards[2]]);
+        }
       });
     }
   }
 
-  let playerCardsValue = playerCards.filter(card => card.value !== "ACE").map(card => cardValueConvert(card.value)).reduce((a,b) => a+b, 0);
-  playerCardsValue = playerCardsValue + playerCards.filter(card => card.value === "ACE").map(card => cardValueConvert(card.value)).reduce((a,b) => a+b, 0);
-  let houseCardsValue = houseCards.filter(card => card.value !== "ACE").map(card => cardValueConvert(card.value)).reduce((a,b) => a+b, 0);
-  houseCardsValue = houseCardsValue + houseCards.filter(card => card.value === "ACE").map(card => cardValueConvert(card.value)).reduce((a,b) => a+b, 0);
+  async function closeGame() {
+    await timeout(500);
+    setEndMode(true);
+  }
+
+  function stand() {
+    hitDealer();
+    closeGame();
+  }
+
+  function hitPlayer() {
+    fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
+    .then(response => response.json())
+    .then(cardData => {
+      setPlayerCards([...playerCards, cardData.cards[0]]);
+    })
+  }
+
+  let playerCardsValue = getHandValue(playerCards);
+  let houseCardsValue = getHandValue(houseCards);
+
+  if (playerCardsValue >= 21 || playerCards.length >= 5) {
+    hitDealer();
+    closeGame();
+  }
+  
+  function winMessage() {
+    if (playerCardsValue > 21) {
+      return "You went over 21! You lose!";
+    }
+    else if (houseCardsValue > 21) {
+      return "Dealer went over 21! You win!";
+    }
+    else if (playerCardsValue === houseCardsValue) {
+      return "It's a tie!";
+    }
+    if (playerCardsValue === 21 && playerCards.length === 2) {
+      return "BLACKJACK! You win!";
+    }
+    else if (playerCardsValue > houseCardsValue) {
+      return "Your cards beat the dealer's. You win!";
+    }
+    else {
+      return "The dealer's cards beat yours. You lose!";
+    }
+  }
+
+  function resetGame() {
+    setPlayerCards([]);
+    setHouseCards([]);
+    setEndMode(false);
+    dealInit();
+  }
 
   if (deckId) {
     return (
       <div>
         <div className={(playerCards.length > 0 && houseCards.length > 0) ? "player-cards" : "hidden"}>
-          <p>Player cards: {playerCards.map(card => card.value).includes("ACE") ? "soft" : ""} {playerCardsValue}</p>
+          <p>Player cards: {playerCardsValue}</p>
           {
             playerCards.map(card => {
               return <img className="card" key={uuid()} src={card.image} alt="card"></img>
             })
           }
-          <button>Hit</button>
-          <button onClick={stand}>Stand</button>
+          <button className={endMode ? "hidden" : ""} onClick={hitPlayer}>Hit</button>
+          <button className={endMode ? "hidden" : ""} onClick={stand}>Stand</button>
         </div>
         <div className={(playerCards.length > 0 && houseCards.length > 0) ? "house-cards" : "hidden"}>
-          <p>House cards</p>
+          <p>Dealers cards {endMode ? `: ${houseCardsValue}`: ""}</p>
           {
             houseCards.slice(0,1).map(card => {
               return <img className="card" key={uuid()} src={card.image} alt="card"></img>
@@ -70,11 +137,17 @@ function Blackjack() {
           }
           {
             houseCards.slice(1).map(card => {
-              return <img className="card" key={uuid()} src={faceDownCard} alt="card"></img>
+              return <img className="card" key={uuid()} src={endMode ? card.image : faceDownCard} alt="card"></img>
             })
           }
         </div>
         <button onClick={dealInit} className={playerCards.length > 0 ? "hidden" : ""} >Deal</button>
+        {
+          (endMode && houseCards.length > 0) ? <p>{winMessage()}</p> : null
+        }
+        {
+          (endMode && houseCards.length > 0) ? <button onClick={resetGame}>Play again</button> : null
+        }
       </div>
     )
   }
--- client/src/components/games/Roulette.js
+++ client/src/components/games/Roulette.js
@@ -1,8 +1,185 @@
-import React from 'react'
+import {React, useState} from 'react'
 
 function Roulette() {
+  const [spin, setSpin] = useState(null);
+
+  function handleTableClick(e) {
+    if ([e.target.className, e.target.parentNode.className].join(' ').includes("clickable")) {
+      console.log(e.target.textContent);
+    }
+  }
+
+  function spinWheel() {
+    fetch('https://www.roulette.rip/api/play?bet=low&wager=200', {
+      method: 'GET',
+      mode: 'no-cors'
+    })
+    .then(response => response.json())
+    .then(spinData => console.log(spinData))
+  }
+
   return (
-    <div>Roulette</div>
+    <div>
+      <div className="roulette-board" onClick={handleTableClick}>
+        <div className="container-first">
+          <div className="clickable zero-item">
+            <div className="value">0</div>
+          </div>
+          <div className="clickable red-item">
+            <div className="value">1</div>
+          </div>
+          <div className="clickable black-item">
+            <div className="value">2</div>
+          </div>
+          <div className="clickable red-item">
+            <div className="value">3</div>
+          </div>
+          <div className="clickable black-item">
+            <div className="value">4</div>
+          </div>
+          <div className="clickable red-item">
+            <div className="value">5</div>
+          </div>
+          <div className="clickable black-item">
+            <div className="value">6</div>
+          </div>
+          <div className="clickable red-item">
+            <div className="value">7</div>
+          </div>
+          <div className="clickable black-item">
+            <div className="value">8</div>
+          </div>
+          <div className="clickable red-item">
+            <div className="value">9</div>
+          </div>
+          <div className="clickable black-item">
+            <div className="value">10</div>
+          </div>
+          <div className="clickable black-item">
+            <div className="value">11</div>
+          </div>
+          <div className="clickable red-item">
+            <div className="value">12</div>
+          </div>
+          <div className="clickable black-item">
+            <div className="value">13</div>
+          </div>
+          <div className="clickable red-item">
+            <div className="value">14</div>
+          </div>
+          <div className="clickable black-item">
+            <div className="value">15</div>
+          </div>
+          <div className="clickable red-item">
+            <div className="value">16</div>
+          </div>
+          <div className="clickable black-item">
+            <div className="value">17</div>
+          </div>
+          <div className="clickable red-item">
+            <div className="value">18</div>
+          </div>
+          <div className="clickable red-item">
+            <div className="value">19</div>
+          </div>
+          <div className="clickable black-item">
+            <div className="value">20</div>
+          </div>
+          <div className="clickable red-item">
+            <div className="value">21</div>
+          </div>
+          <div className="clickable black-item">
+            <div className="value">22</div>
+          </div>
+          <div className="clickable red-item">
+            <div className="value">23</div>
+          </div>
+          <div className="clickable black-item">
+            <div className="value">24</div>
+          </div>
+          <div className="clickable red-item">
+            <div className="value">25</div>
+          </div>
+          <div className="clickable black-item">
+            <div className="value">26</div>
+          </div>
+          <div className="clickable red-item">
+            <div className="value">27</div>
+          </div>
+          <div className="clickable black-item">
+            <div className="value">28</div>
+          </div>
+          <div className="clickable black-item">
+            <div className="value">29</div>
+          </div>
+          <div className="clickable red-item">
+            <div className="value">30</div>
+          </div>
+          <div className="clickable black-item">
+            <div className="value">31</div>
+          </div>
+          <div className="clickable red-item">
+            <div className="value">32</div>
+          </div>
+          <div className="clickable black-item">
+            <div className="value">33</div>
+          </div>
+          <div className="clickable red-item">
+            <div className="value">34</div>
+          </div>
+          <div className="clickable black-item">
+            <div className="value">35</div>
+          </div>
+          <div className="clickable red-item">
+            <div className="value">36</div>
+          </div>
+          <div className="clickable column-item">
+            <div className="value">2-1<p className="hidden"> row1</p></div>
+          </div>
+          <div className="clickable column-item">
+            <div className="value">2-1<p className="hidden"> row2</p></div>
+          </div>
+          <div className="clickable column-item">
+            <div className="value">2-1<p className="hidden"> row3</p></div>
+          </div>
+        </div>
+        <div className="container-second">
+          <div className="clickable doz-item">
+            <div>1st 12</div>
+          </div>
+          <div className="clickable doz-item">
+            <div>2nd 12</div>
+          </div>
+          <div className="clickable doz-item">
+            <div>3rd 12</div>
+          </div>
+        </div>
+        <div className="container-third">
+          <div className="clickable outside-section">
+            <div>1-18</div>
+          </div>
+          <div className="clickable outside-section">
+            <div>EVEN</div>
+          </div>
+          <div className="clickable outside-section">
+            <div className="rhomb-red">red</div>
+          </div>
+          <div className="clickable outside-section">
+            <div className="rhomb-black">black</div>
+          </div>
+          <div className="clickable outside-section">
+            <div>ODD</div>
+          </div>
+          <div className="clickable outside-section">
+            <div>19-36</div>
+          </div>
+        </div>
+      </div>
+      <button onClick={spinWheel}>Spin</button>
+      {/* {
+        spin ? <p>number: {spin.number}, color: {spin.color}, parity: {spin.parity}</p> : null
+      } */}
+    </div>
   )
 }
 
